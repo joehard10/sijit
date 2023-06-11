b18b9af3546ad1aaf7ee94b0e8b2f2da036b41b7
joehard <joel10itoua@gmail.com>
2023-06-12 01:02:43 +0200

    mis a jour de la page d'accueil

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,8 @@
-/*!
-* Start Bootstrap - Agency v7.0.12 (https://startbootstrap.com/theme/agency)
-* Copyright 2013-2023 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-agency/blob/master/LICENSE)
-*/
-//
-// Scripts
-// 
+
+// SPINNER
+setTimeout(function () {
+    $('.loader_bg').fadeToggle();
+}, 6500);
 
 window.addEventListener('DOMContentLoaded', event => {
 
